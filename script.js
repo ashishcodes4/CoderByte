8b74645bf8587a211b2070decafe5e7efa4b8951
Ashish Singh <ashishcodes4@gmail.com>
2018-04-18 08:51:59 +0530

    solution-1

---
+++ script.js
@@ -0,0 +1,17 @@
+// Lok for solution... feel to make changes and submit pull request, if you find something worth changing to make the performance any better. 
+
+// 1.) Calculate the factorila of any given number. 
+function factorial(num) {
+    let fact = 1;
+    for (i=1;i<=num;i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+// 2.) Count the largets word in any given string. 
+
+
+
+
+// 3.) For this challenge you will be reversing a string.
